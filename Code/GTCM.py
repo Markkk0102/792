dc49575e41bc9be33da87c12eb70d68f0b6e851b
Markkk0102 <140997710+Markkk0102@users.noreply.github.com>
2024-06-11 01:17:54 +1200

    Add files via upload

---
+++ Code/GTCM.py
@@ -0,0 +1,180 @@
+import tensorflow.keras.backend as K
+import tensorflow as tf
+import tensorflow.keras.layers
+import numpy as np
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import optimizers
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import Activation, Lambda
+from tensorflow.keras.layers import LeakyReLU, add, ReLU
+from tensorflow.keras.layers import Conv1D, SpatialDropout1D, Dropout
+from tensorflow.keras.layers import Dense, BatchNormalization, GlobalAveragePooling1D
+from tensorflow.keras.activations import sigmoid
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model
+from typing import List, Tuple
+from tensorflow.keras.optimizers import RMSprop
+
+def mul(x):
+    return tf.multiply(x[0], x[1])
+
+def makemean(x):
+    shape = (K.int_shape(x)[1], K.int_shape(x)[2])
+    meantensor = np.ones(shape, dtype=np.float32)
+    meantensor = meantensor / 3
+    meantensor = K.expand_dims(meantensor, 0)
+    meantensor = tf.convert_to_tensor(meantensor)
+    return meantensor
+
+def residual_block(x, s, i, activation, nb_filters, kernel_size, dropout_rate=0, name=''):
+    original_x = x
+    # 第一级
+    # 1.1
+    conv_1_1 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i, padding='causal')(x)
+    conv_1_1 = Activation('relu')(conv_1_1)
+
+    conv_s1_1 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i, padding='causal')(x)
+    conv_s1_1 = Activation('relu')(conv_s1_1)
+    conv_s1_1 = Lambda(sigmoid)(conv_s1_1)
+    output_1_1 = Lambda(lambda x: tf.multiply(x[0], x[1]))([conv_1_1, conv_s1_1])
+    # 1.2
+    conv_1_2 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i, padding='causal')(x)
+    conv_1_2 = Activation('relu')(conv_1_2)
+
+    conv_s1_2 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i, padding='causal')(x)
+    conv_s1_2 = Activation('relu')(conv_s1_2)
+    conv_s1_2 = Lambda(sigmoid)(conv_s1_2)
+    output_1_2 = Lambda(lambda x: tf.multiply(x[0], x[1]))([conv_1_2, conv_s1_2])
+    # 1.3
+    conv_1_3 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i, padding='causal')(x)
+    conv_1_3 = Activation('relu')(conv_1_3)
+
+    conv_s1_3 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i, padding='causal')(x)
+    conv_s1_3 = Activation('relu')(conv_s1_3)
+    conv_s1_3 = Lambda(sigmoid)(conv_s1_3)
+    output_1_3 = Lambda(lambda x: tf.multiply(x[0], x[1]))([conv_1_3, conv_s1_3])
+
+    output_1 = add([output_1_1, output_1_2, output_1_3])
+    templayer = Lambda(makemean)(output_1)
+    output_1 = Lambda(lambda x: tf.multiply(x[0], x[1]))([output_1, templayer])
+
+    # 第二级
+    # 2.1
+    conv_2_1 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i * 2, padding='causal')(output_1)
+    conv_2_1 = Activation('relu')(conv_2_1)
+
+    conv_s2_1 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i * 2, padding='causal')(output_1)
+    conv_s2_1 = Activation('relu')(conv_s2_1)
+    conv_s2_1 = Lambda(sigmoid)(conv_s2_1)
+
+    output_2_1 = Lambda(lambda x: tf.multiply(x[0], x[1]))([conv_2_1, conv_s2_1])
+    # 2.2
+    conv_2_2 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i * 2, padding='causal')(output_1)
+    conv_2_2 = Activation('relu')(conv_2_2)
+
+    conv_s2_2 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i * 2, padding='causal')(output_1)
+    conv_s2_2 = Activation('relu')(conv_s2_2)
+    conv_s2_2 = Lambda(sigmoid)(conv_s2_2)
+
+    output_2_2 = Lambda(lambda x: tf.multiply(x[0], x[1]))([conv_2_2, conv_s2_2])
+    # 2.3
+    conv_2_3 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i * 2, padding='causal')(output_1)
+    conv_2_3 = Activation('relu')(conv_2_3)
+
+    conv_s2_3 = Conv1D(filters=nb_filters, kernel_size=kernel_size, dilation_rate=i * 2, padding='causal')(output_1)
+    conv_s2_3 = Activation('relu')(conv_s2_3)
+    conv_s2_3 = Lambda(sigmoid)(conv_s2_3)
+
+    output_2_3 = Lambda(lambda x: tf.multiply(x[0], x[1]))([conv_2_3, conv_s2_3])
+
+    output_2 = add([output_2_1, output_2_2, output_2_3])
+    templayer1 = Lambda(makemean)(output_2)
+    output_2 = Lambda(lambda x: tf.multiply(x[0], x[1]))([output_2, templayer1])
+    if original_x.shape[-1] != output_2.shape[-1]:
+        original_x = Conv1D(filters=nb_filters, kernel_size=1, padding='same')(original_x)
+    res_x = tf.keras.layers.add([original_x, output_2])
+    return res_x, output_2
+
+class GTCM:
+    """双向 Gated Temporal Convolution Module
+       Args:
+           nb_filters: 使用的卷积层的过滤器数量。
+           kernel_size: 卷积层的内核大小。
+           nb_stacks: 残差块堆栈数量。
+           dilations: 膨胀率列表。
+           activation: 激活函数（例如，relu, leaky_relu）。
+           dropout_rate: dropout比例。
+           use_skip_connections: 是否使用跳过连接。
+           return_sequences: 是否返回完整的序列。
+    """
+    def __init__(self,
+                 nb_filters=64,
+                 kernel_size=2,
+                 nb_stacks=1,
+                 dilations=None,
+                 activation='relu',
+                 use_skip_connections=True,
+                 dropout_rate=0.1,
+                 return_sequences=True,
+                 # name='BiDirectionalGTCM'):
+                 name='GTCM'):
+        self.name = name
+        self.return_sequences = return_sequences
+        self.dropout_rate = dropout_rate
+        self.use_skip_connections = use_skip_connections
+        self.activation = activation
+        self.dilations = dilations
+        self.nb_stacks = nb_stacks
+        self.kernel_size = kernel_size
+        self.nb_filters = nb_filters
+
+        if not isinstance(nb_filters, int):
+            raise Exception()
+
+    def __call__(self, inputs):
+        if self.dilations is None:
+            self.dilations = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+
+        # 前向输入和反向输入
+        forward_input = inputs
+        backward_input = K.reverse(inputs, axes=1)
+
+        # 初始卷积
+        forward_input = Conv1D(self.nb_filters, 1, padding='causal')(forward_input)
+        backward_input = Conv1D(self.nb_filters, 1, padding='causal')(backward_input)
+
+        #forward_skip_connections = []
+        #backward_skip_connections = []
+        final_skip_connection = []
+
+        for s in range(self.nb_stacks):
+            for i in self.dilations:
+                forward_output, skip_out_forward = residual_block(forward_input, s, i, self.activation,
+                                                              self.nb_filters, self.kernel_size,
+                                                              self.dropout_rate, name=self.name + "_forward")
+                backward_output, skip_out_backward = residual_block(backward_input, s, i, self.activation,
+                                                               self.nb_filters, self.kernel_size,
+                                                               self.dropout_rate, name=self.name + "_backward")
+
+                temp_skip = add([skip_out_forward, skip_out_backward], name="biadd_" + str(i))
+                temp_skip = GlobalAveragePooling1D()(temp_skip)
+                temp_skip = tf.expand_dims(temp_skip, axis=1)
+                final_skip_connection.append(temp_skip)
+
+        # 将不同膨胀率的特征组合
+        output_2 = final_skip_connection[0]
+        for i, item in enumerate(final_skip_connection):
+            if i == 0:
+                continue
+            output_2 = K.concatenate([output_2, item], axis=-2)
+        x = output_2
+
+        # 添加激活函数
+        #x = LeakyReLU(alpha=0.05)(x)
+        return x
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[-1], 1)
+
+
+        
---
+++ Code/Model.py
@@ -0,0 +1,255 @@
+import numpy as np
+import tensorflow.keras.backend as K
+import os
+import tensorflow as tf
+from tensorflow.keras.optimizers import SGD,Adam
+from tensorflow.keras import callbacks
+from tensorflow.keras.layers import Layer,Dense,Input
+from tensorflow.keras.models import Model
+from sklearn.metrics import confusion_matrix
+from Common_Model import Common_Model
+from sklearn.model_selection import KFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import datetime
+import pandas as pd
+import copy
+from GTCM import *
+from tensorflow.keras.optimizers import RMSprop
+import matplotlib.pyplot as plt
+from tensorflow.keras import callbacks
+from sklearn.metrics import recall_score, precision_score
+
+def smooth_labels(labels, factor=0.1):
+    # smooth the labels
+    labels *= (1 - factor)
+    labels += (factor / labels.shape[1])
+    return labels
+
+class WeightLayer(Layer):
+    def __init__(self, **kwargs):
+        super(WeightLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=(input_shape[1],1),
+                                      initializer='uniform',
+                                      trainable=True)  
+        super(WeightLayer, self).build(input_shape)  
+ 
+    def call(self, x):
+        tempx = tf.transpose(x,[0,2,1])
+        x = K.dot(tempx,self.kernel)
+        x = tf.squeeze(x,axis=-1)
+        return  x
+ 
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0],input_shape[2])
+    
+def softmax(x, axis=-1):
+    ex = K.exp(x - K.max(x, axis=axis, keepdims=True))
+    return ex/K.sum(ex, axis=axis, keepdims=True)
+
+class TIMNET_Model(Common_Model):
+    def __init__(self, args, input_shape, class_label, **params):
+        super(TIMNET_Model,self).__init__(**params)
+        self.args = args
+        self.data_shape = input_shape
+        self.num_classes = len(class_label)
+        self.class_label = class_label
+        self.matrix = []
+        self.eva_matrix = []
+        self.acc = 0
+        print("TIMNET MODEL SHAPE:",input_shape)
+        
+    def create_model(self):
+        self.inputs=Input(shape = (self.data_shape[0],self.data_shape[1],))
+        self.tcn = GTCM(nb_filters=39,
+                               kernel_size=2, 
+                               nb_stacks=1, #增加堆栈块会增加感受野大小
+                               dilations=[2 ** i for i in range(7)],
+                               activation='relu',
+                               use_skip_connections=True, 
+                               dropout_rate=0.0,
+                               return_sequences=True, 
+                               name='GTCM')(self.inputs)
+        self.Conv1D = GlobalAveragePooling1D()(self.tcn)
+        self.predictions = Dense(self.num_classes,activation='softmax')(self.Conv1D)
+        self.model = Model(inputs = self.inputs,outputs = self.predictions)
+        self.model.compile(loss = 'categorical_crossentropy', 
+                           optimizer = Adam(learning_rate=0.001,beta_1=0.93,beta_2=0.98,epsilon=1e-8), 
+                           metrics = ['accuracy'])
+        
+    def train(self, x, y):
+        filepath = self.args.model_path
+        resultpath = self.args.result_path
+
+        if not os.path.exists(filepath):
+            os.makedirs(filepath)
+        if not os.path.exists(resultpath):
+            os.makedirs(resultpath)
+
+        now = datetime.datetime.now()
+        now_time = datetime.datetime.strftime(now, '%Y-%m-%d_%H-%M-%S')
+        kfold = KFold(n_splits=self.args.split_fold, shuffle=True, random_state=self.args.random_seed)
+        avg_accuracy = 0
+        avg_loss = 0
+        all_recalls = []
+        all_weights = []
+
+        training_histories = []
+
+        for i, (train, test) in enumerate(kfold.split(x, y), start=1):
+            self.create_model()
+            y_train = smooth_labels(copy.deepcopy(y[train]), 0.1)
+            folder_address = os.path.join(filepath, f"{self.args.data}_{self.args.random_seed}_{now_time}")
+            if not os.path.exists(folder_address):
+                os.makedirs(folder_address)
+            weight_path = os.path.join(folder_address, f"{self.args.split_fold}-fold_weights_best_{i}.hdf5")
+            checkpoint = callbacks.ModelCheckpoint(weight_path, verbose=1, save_weights_only=True, save_best_only=False)
+
+            # 记录训练历史
+            history = self.model.fit(
+                x[train], y_train,
+                validation_data=(x[test], y[test]),
+                batch_size=self.args.batch_size, epochs=self.args.epoch,
+                verbose=1, callbacks=[checkpoint]
+            )
+            training_histories.append(history)
+
+            self.model.load_weights(weight_path)
+            best_eva_list = self.model.evaluate(x[test], y[test])
+            avg_loss += best_eva_list[0]
+            avg_accuracy += best_eva_list[1]
+            print(f"{i}_Model evaluation: ", best_eva_list, f"   Now ACC: {round(avg_accuracy * 10000) / 100 / i}")
+
+            y_pred_best = self.model.predict(x[test])
+            cm = confusion_matrix(np.argmax(y[test], axis=1), np.argmax(y_pred_best, axis=1))
+            self.matrix.append(cm)
+            em = classification_report(np.argmax(y[test], axis=1), np.argmax(y_pred_best, axis=1), target_names=self.class_label, output_dict=True)
+            self.eva_matrix.append(em)
+            print(classification_report(np.argmax(y[test], axis=1), np.argmax(y_pred_best, axis=1), target_names=self.class_label))
+
+            # 计算 UAR 和 WAR
+            recalls = recall_score(np.argmax(y[test], axis=1), np.argmax(y_pred_best, axis=1), average=None)
+            weights = [len(np.where(np.argmax(y[test], axis=1) == cls)[0]) for cls in range(len(self.class_label))]
+            all_recalls.append(recalls)
+            all_weights.append(weights)
+
+        # 计算平均 UAR 和 WAR
+        all_recalls = np.array(all_recalls)
+        all_weights = np.array(all_weights)
+        avg_recalls = np.mean(all_recalls, axis=0)
+        uar = np.mean(avg_recalls)
+        war = np.sum(avg_recalls * np.sum(all_weights, axis=0)) / np.sum(all_weights)
+
+        avg_acc = avg_accuracy / self.args.split_fold
+        print("Average ACC:", avg_acc)
+        print("Average UAR:", uar)
+        print("Average WAR:", war)
+
+        result_file = os.path.join(resultpath, f"{self.args.data}_{self.args.split_fold}fold_{round(avg_acc * 10000) / 100}_{self.args.random_seed}_{now_time}.xlsx")
+        with pd.ExcelWriter(result_file) as writer:
+            for i, item in enumerate(self.matrix):
+                temp = {" ": self.class_label}
+                for j, l in enumerate(item):
+                    temp[self.class_label[j]] = item[j]
+                data1 = pd.DataFrame(temp)
+                data1.to_excel(writer, sheet_name=str(i))
+
+                df = pd.DataFrame(self.eva_matrix[i]).transpose()
+                df.to_excel(writer, sheet_name=f"{i}_evaluate")
+
+        K.clear_session()
+        self.matrix = []
+        self.eva_matrix = []
+        self.acc = 0
+        self.trained = True
+
+        # 绘制并保存训练历史图
+        self.plot_training_history(training_histories, resultpath, now_time)
+
+    def plot_training_history(self, histories, resultpath, timestamp):
+        for i, history in enumerate(histories, start=1):
+            plt.figure(figsize=(12, 4))
+
+            # Loss 图
+            plt.subplot(1, 2, 1)
+            plt.plot(history.history['loss'], label='Train Loss')
+            plt.plot(history.history['val_loss'], label='Validation Loss')
+            plt.title(f'Fold {i} Loss')
+            plt.xlabel('Epochs')
+            plt.ylabel('Loss')
+            plt.legend()
+
+            # Accuracy 图
+            plt.subplot(1, 2, 2)
+            plt.plot(history.history['accuracy'], label='Train Accuracy')
+            plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+            plt.title(f'Fold {i} Accuracy')
+            plt.xlabel('Epochs')
+            plt.ylabel('Accuracy')
+            plt.legend()
+
+            plt.tight_layout()
+
+            # 保存图像到文件
+            img_filename = os.path.join(resultpath, f"{self.args.data}_{self.args.split_fold}fold_{timestamp}_fold_{i}.png")
+            plt.savefig(img_filename)
+
+            # 显示图像
+            plt.show()
+
+            plt.close()  # 关闭当前图形，避免内存泄漏
+    
+    def test(self, x, y, path):
+        i = 1
+        kfold = KFold(n_splits=self.args.split_fold, shuffle=True, random_state=self.args.random_seed)
+        avg_accuracy = 0
+        avg_loss = 0
+        x_feats = []
+        y_labels = []
+        all_recalls = []
+        all_weights = []
+
+        for train, test in kfold.split(x, y):
+            self.create_model()
+            weight_path = path + '/' + str(self.args.split_fold) + "-fold_weights_best_" + str(i) + ".hdf5"
+            self.model.fit(x[train], y[train], validation_data=(x[test], y[test]), batch_size=64, epochs=0, verbose=0)
+            self.model.load_weights(weight_path)  # +source_name+'_single_best.hdf5')
+            best_eva_list = self.model.evaluate(x[test], y[test])
+            avg_loss += best_eva_list[0]
+            avg_accuracy += best_eva_list[1]
+            print(str(i) + '_Model evaluation: ', best_eva_list, "   Now ACC:", str(round(avg_accuracy * 10000) / 100 / i))
+            i += 1
+            y_pred_best = self.model.predict(x[test])
+            cm = confusion_matrix(np.argmax(y[test], axis=1), np.argmax(y_pred_best, axis=1))
+            self.matrix.append(cm)
+            em = classification_report(np.argmax(y[test], axis=1), np.argmax(y_pred_best, axis=1), target_names=self.class_label, output_dict=True)
+            self.eva_matrix.append(em)
+            print(classification_report(np.argmax(y[test], axis=1), np.argmax(y_pred_best, axis=1), target_names=self.class_label))
+            caps_layer_model = Model(inputs=self.model.input, outputs=self.model.get_layer(index=-2).output)
+            feature_source = caps_layer_model.predict(x[test])
+            x_feats.append(feature_source)
+            y_labels.append(y[test])
+
+            # 计算 UAR 和 WAR
+            recalls = recall_score(np.argmax(y[test], axis=1), np.argmax(y_pred_best, axis=1), average=None)
+            weights = [len(np.where(np.argmax(y[test], axis=1) == cls)[0]) for cls in range(len(self.class_label))]
+            all_recalls.append(recalls)
+            all_weights.append(weights)
+
+        # 计算平均 UAR 和 WAR
+        all_recalls = np.array(all_recalls)
+        all_weights = np.array(all_weights)
+        avg_recalls = np.mean(all_recalls, axis=0)
+        uar = np.mean(avg_recalls)
+        war = np.sum(avg_recalls * np.sum(all_weights, axis=0)) / np.sum(all_weights)
+
+        print("Average ACC:", avg_accuracy / self.args.split_fold)
+        print("Average UAR:", uar)
+        print("Average WAR:", war)
+
+        self.acc = avg_accuracy / self.args.split_fold
+        return x_feats, y_labels
+
